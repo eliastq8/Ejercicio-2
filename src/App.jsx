21372608273b16f7788d13fc97627f72163ade3b
eliastq8 <elias.leonardorivera@umg.edu.mx>
2024-04-18 22:32:44 -0600

    Ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
        </Route>
-       
       </Routes>
     </>
   )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,15 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
     return <>            
     <p>Nombre: VALERIA BAÑUELOS MONTEVERDE, ELIAS LEONARDO RIVERA</p>
     <p>Fecha: 18/04/24</p>
     <p>Materia: Programación Avanzada</p>
     <p>Segundo parcial</p>
-    <a href="http://localhost:5173/profile" >Profile</a>
+    <Button onClick ={() => navigate("/profile")}>Profile</Button>
     </>;
 
 }
